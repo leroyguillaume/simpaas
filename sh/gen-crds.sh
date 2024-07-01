4f4cb65cbb9ba6061875107cd0dee92848322fe0
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2024-07-01 14:18:14 +0200

    fix(helm/simpaas): make user creation idempotent

--- sh/gen-crds.sh
+++ sh/gen-crds.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+cargo build
 mkdir -p charts/simpaas/crds
 for crd in app invit role user; do
-  cargo run -- crd $crd > charts/simpaas/crds/$crd.yaml
+  target/debug/simpaas crd $crd > charts/simpaas/crds/$crd.yaml
 done
 git diff --exit-code charts/simpaas/crds/*.yaml
